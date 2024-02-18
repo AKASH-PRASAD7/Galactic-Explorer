0e39a20e3854922d4ad5ef3014cead26dc6b7cca
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-18 11:06:59 +0530

    Shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Galactic Explorer
-    </main>
-  );
+  return <main>Galactic Explorer</main>;
 }
