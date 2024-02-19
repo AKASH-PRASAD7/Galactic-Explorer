c844f5571a07529cb81261935646cc9553e2f6b6
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-02-19 17:44:37 +0530

    minor fixes

--- src/app/about/[category]/[id]/page.tsx
+++ src/app/about/[category]/[id]/page.tsx
@@ -1,37 +1,11 @@
-"use client";
 import Image from "next/image";
 import star from "@/assests/starwars.jpg";
-import React, { useEffect, useState } from "react";
 
+import About from "@/components/About";
 import Link from "next/link";
-
-import filterType from "@/utils/filtertType";
 
 const page = ({ params }: any): JSX.Element => {
   const { category, id } = params;
-  const [error, setError] = useState<string>("");
-  const [data, setData] = useState<any>({});
-
-  const getData = async (cat: string, ind: string) => {
-    try {
-      const res = await fetch(`http://localhost:3000/api/get/${cat}/${ind}`);
-      const resData = await res.json();
-
-      if (resData?.data?.detail === "Not found") {
-        setError(`Following ${cat} details not found`);
-      }
-
-      const res1 = filterType(resData.data, cat);
-      setData(res1);
-    } catch (err) {
-      setError("Error fetching: " + err);
-    }
-  };
-
-  useEffect(() => {
-    setError("");
-    getData(category, id);
-  }, [category, id]);
 
   return (
     <main className="relative min-h-screen">
@@ -46,30 +20,7 @@ const page = ({ params }: any): JSX.Element => {
             Galactic Explorer
           </h1>
         </Link>
-        <div>
-          {error ? (
-            <p className="m-8 text-center font-xl font-semibold text-red-500">
-              {error}
-            </p>
-          ) : (
-            <>
-              <div className="max-w-md mx-auto  p-6 rounded-md ">
-                <h2 className="text-2xl font-semibold mb-4">
-                  Character Details
-                </h2>
-
-                <div className="flex flex-col glass-table p-8 text-white">
-                  {Object.keys(data).map((item, index: number) => (
-                    <p key={index} className="text-lime-500">
-                      <span className="font-semibold text-white">{item}:</span>{" "}
-                      {data[item]}
-                    </p>
-                  ))}
-                </div>
-              </div>
-            </>
-          )}
-        </div>
+        <About category={category} id={id} />
       </section>
     </main>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import filterType from "@/utils/filtertType";
+
+interface params {
+  category: string;
+  id: string;
+}
+
+const About = ({ category, id }: params): JSX.Element => {
+  const [error, setError] = useState<string>("");
+  const [data, setData] = useState<any>({});
+
+  const getData = async (cat: string, ind: string) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/get/${cat}/${ind}`);
+      const resData = await res.json();
+
+      if (resData?.data?.detail === "Not found") {
+        setError(`Following ${cat} details not found`);
+      }
+
+      const res1 = filterType(resData.data, cat);
+      setData(res1);
+    } catch (err) {
+      setError("Error fetching: " + err);
+    }
+  };
+
+  useEffect(() => {
+    setError("");
+    getData(category, id);
+  }, [category, id]);
+  return (
+    <div>
+      {error ? (
+        <p className="m-8 text-center font-xl font-semibold text-red-500">
+          {error}
+        </p>
+      ) : (
+        <>
+          <div className="max-w-md mx-auto  p-6 rounded-md ">
+            <h2 className="text-2xl font-semibold mb-4">Character Details</h2>
+
+            <div className="flex flex-col glass-table p-8 text-white">
+              {Object.keys(data).map((item, index: number) => (
+                <p key={index} className="text-lime-500">
+                  <span className="font-semibold text-white">{item}:</span>{" "}
+                  {data[item]}
+                </p>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default About;
